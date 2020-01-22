eebf5994e869aa261f54ca3545de1e362f3d60ba
Luis8811 <lsuarezg8811@gmail.com>
2020-01-22 17:36:29 +0100

    basic skeleton of quiz

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+   countries: string[] = ["Federación de Rusia", "Cuba", "India", "Italia", "Suiza", "Noruega", "Venezuela"];
+   cities: string[][] = [["Kiev", "Moscú", "Volvogrado"], ["La Habana", "Matanzas", "Santo Domingo"], ["Nueva Delhi",
+    "Bengala", "Calcuta"], ["Milán", "Florencia", "Roma"], ["Berna", "Zurich", "Colonia"], ["Oslo", "Estocolmo", "Copenhague"], 
+    ["Quito", "Caracas", "Bogotá"]];
+    currentIndex: number = 0;
+    answers: string[] = ["Moscú", "La Habana", "Nueva Delhi", "Roma", "Berna", "Oslo", "Caracas"];
+    selected: string[] = [];
+    currentSelection: string = "";
+    currentCity = this.cities[0][0];
 
   constructor() {}
+
+  nextQuestion(optionSelected) {
+    console.log(optionSelected + ' was selected!')
+    this.selected.push(optionSelected);
+    this.currentIndex++;
+    this.currentCity = this.cities[this.currentIndex][0];
+    this.currentSelection = "";
+  }
 
 }
