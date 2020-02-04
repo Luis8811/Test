171bcad31170ea378a9652f67afabb1b08684483
Luis8811 <lsuarezg8811@gmail.com>
2020-02-04 20:03:00 +0100

    Cleaning up the code

--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -7,59 +7,44 @@ import { Chart } from 'chart.js';
   styleUrls: ['./results.page.scss'],
 })
 export class ResultsPage implements OnInit {
-  @ViewChild('barChart', null) barChart;
+  @ViewChild('answersChart', null) barChart;
   bars: any;
   colorArray: any;
+  nameOfTest: string = 'Capitales del mundo';
+  answersLabel: string = 'Respuestas';
+  titleLabel: string = 'Resultados del test';
   constructor() { }
 
   ionViewDidEnter() {
-    this.createBarChart();
+    this.createChart();
   }
 
   ngOnInit() {
   }
 
-  createBarChart() {
+  createChart() {
     this.bars = new Chart(this.barChart.nativeElement, {
       type: 'doughnut',
       data: {
-        labels: ['S1', 'S2', 'S3', 'S4'],
+        labels: ['Respuestas incorrectas', 'Respuestas correctas'],
         datasets: [{
           label: 'Viewers in millions',
-          data: [2.5, 3.8, 5, 6.9],
+          data: [5, 4],
           backgroundColor: [
             'rgba(255, 99, 132, 0.5)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)'
+            'rgba(54, 162, 235, 0.2)'
           ],
           borderColor: [
             'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)'
+            'rgba(54, 162, 235, 1)'
           ],
           borderWidth: 1
-         //,
-         /*  backgroundColor: ['rgb(38, 194, 129)'], // array should have same number of elements as number of dataset
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-          borderWidth: 1 */
         }]
       },
       options: {
         cutoutPercentage: 40,
        responsive: false,
-   
-     }//,
-      /* options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      } */
+     }
     });
   }
 
