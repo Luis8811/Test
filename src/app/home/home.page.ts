06b50fbb8b90fe5c2ded70966972dc3b87dd25f5
Luis8811 <lsuarezg8811@gmail.com>
2020-02-18 18:31:18 +0100

    Passing data to page of results

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,12 +16,15 @@ export class HomePage {
     selected: string[] = [];
     currentSelection: string = "";
     currentCity = this.cities[0][0];
+    badAnswers: number = 0;
+    goodAnswers: number = 0;
 
   constructor(private router: Router) {}
 
   nextQuestion(optionSelected) {
     console.log(optionSelected + ' was selected!')
     this.selected.push(optionSelected);
+    this.updateScore(this.currentIndex, optionSelected);
     this.currentCity = this.cities[this.currentIndex][0];
     this.currentSelection = "";
     this.currentIndex++;
@@ -32,7 +35,28 @@ export class HomePage {
 
   showResults() {
     console.log('navigation to page of results:');
-    this.router.navigate(['/results']);
+    let currentBadAnswers = this.badAnswers;
+    let currentGoodAnswers = this.goodAnswers;
+    this.router.navigate(['/results', {bad: currentBadAnswers, good: currentGoodAnswers}]);
   }
+
+  initTest() {
+    this.currentIndex = 0;
+    this.selected = [];
+    this.currentSelection = "";
+    this.currentCity = this.cities[0][0];
+    this.badAnswers = 0;
+    this.goodAnswers = 0;
+  }
+
+  updateScore(indexOfQuestion: number, selectedOption: string) {
+    if (this.answers[indexOfQuestion] != selectedOption) {
+      this.badAnswers++;
+    } else {
+      this.goodAnswers++;
+    }
+  }
+
+  
 
 }
--- src/app/results/results.page.ts
+++ src/app/results/results.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
+import {NavController} from '@ionic/angular';
+import {HomePage} from '../home/home.page';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-results',
@@ -13,13 +16,26 @@ export class ResultsPage implements OnInit {
   nameOfTest: string = 'Capitales del mundo';
   answersLabel: string = 'Respuestas';
   titleLabel: string = 'Resultados del test';
-  constructor() { }
+  badAnswers: number = 0;
+  goodAnswers: number = 0;
+  constructor(public navController: NavController,  private route: ActivatedRoute,
+    private router: Router) { }
 
   ionViewDidEnter() {
     this.createChart();
   }
 
+  goBack() {
+    this.navController.pop();
+  }
+
   ngOnInit() {
+    let bad = this.route.snapshot.paramMap.get('bad');
+    let good = this.route.snapshot.paramMap.get('good');
+    console.log('Bad answers: ' + bad);
+    console.log('Good answers: ' + good);
+    this.badAnswers = parseInt(bad);
+    this.goodAnswers = parseInt(good);
   }
 
   createChart() {
@@ -29,7 +45,7 @@ export class ResultsPage implements OnInit {
         labels: ['Respuestas incorrectas', 'Respuestas correctas'],
         datasets: [{
           label: 'Viewers in millions',
-          data: [5, 4],
+          data: [this.badAnswers, this.goodAnswers],
           backgroundColor: [
             'rgba(255, 99, 132, 0.5)',
             'rgba(54, 162, 235, 0.2)'
